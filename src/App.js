f8a30169d5728cf1f7d783ed9260d5f699bb01cd
Naveen <catchspider2002@gmail.com>
2019-09-20 02:33:59 +0530

    Added progress bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
 import Card from "./Molecules/MoleculeCard";
-import "./bootstrap-custom.css"
+import Progress from "./Molecules/MoleculeProgress";
+import "./bootstrap-custom.css";
 
 const App = () => (
     <div className="App">
         <header className="App-header">
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-      <Card title="New title" subtitle="Sub" />
+            <Progress />
+            <Card title="New title" subtitle="Sub" />
         </header>
     </div>
 );
